e88a6e40dd30bb66b2de89068078afbfd1c85ee8
Wushengyouhan <840835067@qq.com>
2024-11-23 20:41:04 +0800

    feat: 104. Outputting Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -18,11 +20,15 @@ function App() {
       };
     });
   }
+
   return (
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
